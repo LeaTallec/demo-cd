62fe74e2a71549c508206c010354af5fd9decebb
LeaTallec <LeaTallec@users.noreply.github.com>
2025-03-28 13:38:03 +0000

    deploy: 51735665899ae54cd7b3887790e8e261a8001c69

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("Ok : ça fonctionne");
+
+const url = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=40"
+
+fetch(url)
+.then(reponse => reponse.json())
+.then(pokemons => {
+    console.log(pokemons)
+})
